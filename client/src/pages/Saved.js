d6cbffb564f76eb81b328419c450e4586c824546
Christian Sinai <christian_sinai@me.com>
2021-01-07 11:08:09 -0800

    initial commit

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Form, InputGroup, Button } from 'react-bootstrap';
+
+import API from '../utils/API';
+
+function Saved() {
+	return (
+		<>
+			Saved Books
+		</>
+	);
+};
+
+export default Saved;
