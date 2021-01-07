6adb447241d8fbfcddd513c1593ba32bad0082d0
Christian Sinai <christian_sinai@me.com>
2021-01-07 11:04:38 -0800

    production/ build

---
+++ client/build/precache-manifest.8523c5529098b7b3bcf55378a457b8c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab812e5f9d714d3ae9f4226754adb35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76026fa4906ef3b29f7a",
+    "url": "/static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "76026fa4906ef3b29f7a",
+    "url": "/static/js/2.4e901fa9.chunk.js"
+  },
+  {
+    "revision": "a04c66e1a97d8adc8161745ab8066634",
+    "url": "/static/js/2.4e901fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0162eaba93ee5ea82f",
+    "url": "/static/js/main.60439497.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
